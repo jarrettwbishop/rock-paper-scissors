b6c71f271c2449161340b2af60659237add8a961
Jarrett <jarrettwbishop@gmail.com>
2022-11-19 21:52:46 -0500

    Complete console game of Rock, Paper, Scissors

--- index.js
+++ index.js
@@ -1,6 +1,67 @@
 function getComputerChoice() {
-    let num = Math.floor(math.random(3))
+    let num = Math.floor(Math.random() * 3)
     return num === 0 ? "Rock" : num === 1 ? "Paper" : "Scissors"
 }
 
-console.log(getComputerChoice);
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === "rock") {
+        switch(computerSelection) {
+            case "Rock":
+                console.log("It's a tie!");
+                return "tie"
+            case "Paper":
+                console.log("You lose! Paper beats rock.");
+                return "lose"
+
+            case "Scissors":
+                console.log("You win! Rock beats scissors.");
+                return "win"
+
+        }   
+    }
+
+    if (playerSelection.toLowerCase() === "paper") {
+        switch(computerSelection) {
+            case "Rock":
+                console.log("You win! Paper beats rock.");
+                return "win"
+
+            case "Paper":
+                console.log("It's a tie!");
+                return "tie"
+
+            case "Scissors":
+                console.log("You lose! Scissors beats paper.");
+                return "lose"
+
+        }   
+    }
+
+    if (playerSelection.toLowerCase() === "scissors") {
+        switch(computerSelection) {
+            case "Rock":
+                console.log("You lose! Rock beats scissors.");
+                return "lose"
+
+            case "Paper":
+                console.log("You win! Scissors beats paper.");
+                return "win"
+
+            case "Scissors":
+                console.log("It's a tie!");
+                return "tie"
+
+        }   
+    }
+}
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        const playerChoice = prompt("Rock, Paper, or Scissors?");
+        let result = playRound(playerChoice,getComputerChoice())
+        result === "lose" ? computerScore++ : result === "win" ? playerScore++ : playerScore = playerScore
+    }
+    return playerScore > computerScore ? "You won best of 5!" : playerScore < computerScore ? "You lost best of 5!" : "You tied best of 5!"
+}
