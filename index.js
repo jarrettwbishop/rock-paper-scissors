2c1387bcb46c6af407802f17cca8e06623ec772a
Jarrett <jarrettwbishop@gmail.com>
2022-11-24 10:58:18 -0500

    Create basic UI for RPS game

--- index.js
+++ index.js
@@ -1,21 +1,89 @@
+let compScore = 0;
+let playerScore = 0;
+let results = document.querySelector("#results");
+
+const compScoreEl = document.querySelector('#comp-score');
+const playerScoreEl = document.querySelector('#player-score');
+const btnRock = document.querySelector('#btn-rock');
+const btnPaper = document.querySelector('#btn-paper');
+const btnScissors = document.querySelector('#btn-scissors');
+const btnNewGame = document.querySelector('#new-game');
+
+compScoreEl.textContent = "Computer Score: " + compScore
+playerScoreEl.textContent = "Player Score: " + playerScore
+
+btnRock.addEventListener('click', () => {
+    playRound("Rock", getComputerChoice());
+})
+btnPaper.addEventListener('click', () => {
+    playRound("Paper", getComputerChoice());
+})
+btnScissors.addEventListener('click', () => {
+    playRound("Scissors", getComputerChoice());
+})
+btnNewGame.addEventListener('click', () => {
+    newGame();
+} )
+
 function getComputerChoice() {
     let num = Math.floor(Math.random() * 3)
     return num === 0 ? "Rock" : num === 1 ? "Paper" : "Scissors"
+}
+
+function disableBtn() {
+    btnRock.disabled = true
+    btnPaper.disabled = true
+    btnScissors.disabled = true
+}
+
+function enableBtn() {
+    btnRock.disabled = false
+    btnPaper.disabled = false
+    btnScissors.disabled = false
+}
+
+function updateScore() {
+    compScoreEl.textContent = "Computer Score: " + compScore
+    playerScoreEl.textContent = "Player Score: " + playerScore
+
+    if (playerScore === 5) {
+        results.textContent = "Congratulations, you won the game!"
+        disableBtn();
+        
+    }
+    if (compScore === 5) {
+        results.textContent = "You lost the game! Better luck next time."
+        disableBtn();
+    }
+    if (playerScore === 0 && compScore === 0) {
+        results.textContent = "Choose your weapon!"
+        enableBtn();
+    }
+}
+
+function newGame() {
+    playerScore = 0;
+    compScore = 0;
+    updateScore();
 }
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() === "rock") {
         switch(computerSelection) {
             case "Rock":
-                console.log("It's a tie!");
-                return "tie"
+                results.textContent = "It's a tie!";
+                break;
             case "Paper":
-                console.log("You lose! Paper beats rock.");
-                return "lose"
+                results.textContent = "You lose! Paper beats rock.";
+                compScore++;
+                updateScore();
+                break;
 
             case "Scissors":
-                console.log("You win! Rock beats scissors.");
-                return "win"
+                results.textContent = "You win! Rock beats scissors.";
+                playerScore++;
+                updateScore();
+                break;
 
         }   
     }
@@ -23,16 +91,20 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() === "paper") {
         switch(computerSelection) {
             case "Rock":
-                console.log("You win! Paper beats rock.");
-                return "win"
+                results.textContent = "You win! Paper beats rock.";
+                playerScore++;
+                updateScore();
+                break;
 
             case "Paper":
-                console.log("It's a tie!");
-                return "tie"
+                results.textContent = "It's a tie!";
+                break;
 
             case "Scissors":
-                console.log("You lose! Scissors beats paper.");
-                return "lose"
+                results.textContent = "You lose! Scissors beats paper.";
+                compScore++;
+                updateScore();
+                break;
 
         }   
     }
@@ -40,28 +112,21 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() === "scissors") {
         switch(computerSelection) {
             case "Rock":
-                console.log("You lose! Rock beats scissors.");
-                return "lose"
+                results.textContent = "You lose! Rock beats scissors.";
+                compScore++;
+                updateScore();
+                break;
 
             case "Paper":
-                console.log("You win! Scissors beats paper.");
-                return "win"
+                results.textContent = "You win! Scissors beats paper.";
+                playerScore++;
+                updateScore();
+                break;
 
             case "Scissors":
-                console.log("It's a tie!");
-                return "tie"
+                results.textContent = "It's a tie!";
+                break;
 
         }   
     }
-}
-
-function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        const playerChoice = prompt("Rock, Paper, or Scissors?");
-        let result = playRound(playerChoice,getComputerChoice())
-        result === "lose" ? computerScore++ : result === "win" ? playerScore++ : playerScore = playerScore
-    }
-    return playerScore > computerScore ? "You won best of 5!" : playerScore < computerScore ? "You lost best of 5!" : "You tied best of 5!"
 }
